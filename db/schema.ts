c66c029091e30e423ef2da0ee54391483aace27f
Finn Joshua Bartels <kontakt@fiosproject.de>
2025-11-14 15:00:08 +0100

    add drizzle and getRandomPoll()

---
+++ db/schema.ts
@@ -0,0 +1,24 @@
+import { relations } from "drizzle-orm";
+import { pgTable, text, serial, varchar, integer } from "drizzle-orm/pg-core";
+
+export const questions = pgTable("questions", {
+  id: serial("id").primaryKey(),
+  question: text()
+})
+
+export const questionsRelations = relations(questions, ({ many }) => ({
+  answers: many(answers)
+}))
+
+export const answers = pgTable("answers", {
+  id: serial("id").primaryKey(),
+  answer: varchar({ length: 256 }),
+  questionId: integer("question_id")
+})
+
+export const answersRelations = relations(answers, ({ one }) => ({
+  question: one(questions, {
+    fields: [answers.questionId],
+    references: [questions.id]
+  })
+}))
---
+++ lib/drizzle/getRandomPoll.ts
@@ -0,0 +1,19 @@
+import * as schema from '@/db/schema';
+import { count } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+export async function getRandomPoll() {
+  const db = drizzle(process.env.DATABASE_URL!, { schema });
+
+  const totalQuestions = await db.select({ count: count() }).from(schema.questions)
+
+  const result = await db.query.questions.findMany({
+    limit: 1,
+    offset: Math.floor(Math.random() * totalQuestions[0].count),
+    with: {
+      answers: true
+    },
+  });
+
+  return result
+}
