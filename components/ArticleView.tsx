5a2b8c64c6b560c5a431fae8d2810b7dcd978f28
Finn Joshua Bartels <kontakt@fiosproject.de>
2025-12-21 14:26:43 +0100

    style: fix "Read more" button always bottom right

--- components/ArticleView.tsx
+++ components/ArticleView.tsx
@@ -26,7 +26,7 @@ export default async function ArticleView({
               key={post.id}
               className="
               overflow-hidden flex h-full w-full max-w-3xl flex-row
-              glass rounded-4xl mb-4
+              glass rounded-4xl mb-4 min-h-64
               "
             >
               {post.featuredMediaAvailable && imageSize && (
@@ -38,15 +38,20 @@ export default async function ArticleView({
                   alt={post.featuredMedia?.alt || ""}
                 />
               )}
-              <div className="w-full h-max rounded p-4">
-                <h2 className="text-blue-950 antialiased font-bold font-3xl md:text-xl lg:text-2xl mb-2">
-                  {post.title}
-                </h2>
-                <Categories slug={post.slug} />
-                <PostExcerpt html={post.excerpt} />
-                <Button href={"/artikel/" + post.slug}>
-                  Weiterlesen
-                </Button>
+              <div className="w-full flex flex-col justify-between p-4 ">
+                <div className="space-y-2">
+                  <h2 className="text-blue-950 antialiased font-bold font-3xl md:text-xl lg:text-2xl mb-2">
+                    {post.title}
+                  </h2>
+                  <Categories slug={post.slug} />
+                  <PostExcerpt html={post.excerpt} />
+                </div>
+                <div className="flex justify-end">
+                  <Button href={"/artikel/" + post.slug}>
+                    Weiterlesen
+                  </Button>
+
+                </div>
               </div>
             </li>
           )
