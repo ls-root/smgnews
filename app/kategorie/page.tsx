aa0be07981dc111d1278cd6fbca7e8ba7a0a633e
Finn Joshua Bartels <kontakt@fiosproject.de>
2025-12-21 14:33:59 +0100

    style: /kategorie route

--- app/kategorie/page.tsx
+++ app/kategorie/page.tsx
@@ -1,22 +1,22 @@
+import Button from "@/components/Button"
 import SetHeader from "@/components/SetHeader"
 import getCategories from "@/lib/wpRest/getCategories"
 import wpLinkToRoute from "@/utils/wpLinkToRoute"
-import Link from "next/link"
 
 export default async function KategoriePage() {
   const categories = await getCategories()
 
   return (
     <>
-      <h1>Alle Kategorien</h1>
       <SetHeader title="Kategorien" subtitle="Hier findest du alle Kategorien" />
-      {categories.map(category => (
-        <Link
-          key={category.link}
-          href={wpLinkToRoute("category", category.link)}
-          className="border border-black"
-        > {"#" + category.name + "(" + category.slug + ")"} </Link>
-      ))}
+      <div className="space-y-2">
+        {categories.map(category => (
+          <Button
+            key={category.link}
+            href={wpLinkToRoute("category", category.link)}
+          > {category.name} </Button>
+        ))}
+      </div>
     </>
   )
 }
