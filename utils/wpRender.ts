1c0a685bed88b6ae8ec542f4171045186a92b5de
Finn Joshua Bartels <kontakt@fiosproject.de>
2025-12-31 10:54:29 +0100

    wp-renderer: cover background tinting

--- utils/wpRender.ts
+++ utils/wpRender.ts
@@ -1,11 +1,13 @@
 import * as cheerio from "cheerio";
 import captionOutsideTable from "./wpRender/captionOutsideTable";
+import backgroundDim from "./wpRender/backgroundDim";
 
 // parts of the wordpress renderer are located at /app/globals.css
 export default function wpRender(html: string) {
   const $ = cheerio.load(html)
 
   captionOutsideTable($)
+  backgroundDim($)
 
   return $.html()
 }
