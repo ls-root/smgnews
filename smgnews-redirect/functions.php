2156bad195e82d342025cc5c910c0dd2c5b09ec6
Finn Joshua Bartels <kontakt@fiosproject.de>
2026-01-11 13:58:23 +0100

    add: smgnews-redirect theme for smart redirects in wordpress

---
+++ smgnews-redirect/functions.php
@@ -0,0 +1,33 @@
+<?php
+
+add_theme_support('post-thumbnails');
+
+add_action('template_redirect', function () {
+    if (
+        is_preview()
+    ) {
+        echo '<div style="max-width:700px;margin:4rem auto;font-family:sans-serif">';
+        echo '<h1>Nicht verfügbar</h1>';
+        echo '<p>Diese funktion wurde noch nicht implementiert du kannst aber helfen preview funktionalitäten im smgnews-redirect theme hinzuzufügen</p>';
+        echo '<p>Source: <a href="https://github.com/ls-root/smgnews/tree/main/smgnews-redirect">https://github.com/ls-root/smgnews/tree/main/smgnews-redirect</a></p>';
+        echo '</div>';
+    }
+
+    $base_target = 'http://localhost:3000';
+
+    if (is_singular('post')) {
+        global $post;
+
+        if (!$post) {
+            return;
+        }
+
+        $target_url = $base_target . '/artikel/' . $post->post_name;
+
+        wp_redirect($target_url, 301);
+        exit;
+    }
+
+    wp_redirect($base_target, 301);
+    exit;
+});
