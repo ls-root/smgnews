a276c1f8228b10c3669fbede7a0971642fd709d3
Finn Joshua Bartels <kontakt@fiosproject.de>
2026-06-22 17:46:31 +0200

    revert parts of 28e9f59

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      new URL("https://localhost/*"),
       new URL(`https://wp.fiosproject.de/wp-content/uploads/**`),
       new URL("https://secure.gravatar.com/avatar/**"),
       new URL("https://localhost:8080/wp-content/uploads/**"),
@@ -12,12 +11,6 @@ const nextConfig: NextConfig = {
         hostname: 'secure.gravatar.com',
         pathname: '/avatar/**',
         search: "?s=96&d=mm&r=g"
-      },
-      {
-        protocol: "https",
-        hostname: "127.0.0.1",
-        port: "8080",
-        pathname: "**",
       }
     ]
   },
